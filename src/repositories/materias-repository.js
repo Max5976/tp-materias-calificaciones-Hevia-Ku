17c9a1a10e8a7b95b1f13843bc8d252191b461b1
Android Studio <devandroid@ort>
2026-05-29 08:43:10 -0300

    Mejorando y aprendiendo

--- src/repositories/materias-repository.js
+++ src/repositories/materias-repository.js
@@ -1,1 +1,11 @@
 import Db from './db-pg.js';
+
+export default class MateriaRepository {
+
+    getMaterias = async () => {
+        console.log(`MateriaRepository.getAllAsync()`);
+        const sql = `SELECT * FROM cursos`;
+        return await this.db.queryAll(sql);
+    }
+}
+
